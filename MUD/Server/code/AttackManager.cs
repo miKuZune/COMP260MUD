0ef8fdab867b480edfbd13f2a2bde52c67a6281e
mikuzune <maxwfarley@live.co.uk>
2018-02-24 19:13:45 +0000

    Split up program into new files. Added comments to program and to the new files.

---
+++ MUD/Server/code/AttackManager.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Server
+{
+    public class AttackManager
+    {
+        public AttackManager(Player player, Enemy enemy)
+        {
+            this.player = player;
+            this.enemy = enemy;
+        }
+
+        //Player deals damage to the enemy and a message is sent back.
+        public string PlayerAttack(int damageToDeal)
+        {
+            string messageBack = null;
+
+            enemy.enemyHealth.TakeHealth(damageToDeal);
+
+            messageBack = player.name + " has attacked " + enemy.GetName() + " for " + damageToDeal + ".";
+
+            return messageBack;
+        }
+        //Gets the enemies ID from the list of enemies in the room.
+        int GetEnemeyPosInList()
+        {
+            int id = 0;
+
+            for(int i = 0; i < player.currRoom.enemyList.Count; i++)
+            {
+                if(player.currRoom.enemyList[i] == enemy)
+                {
+                    id = i;
+                }
+            }
+
+            return id;
+        }
+
+        //Checks if there is more than 0 enemies in the room.
+        bool EnemiesLeftInRoom()
+        {
+            if(player.currRoom.enemyList.Count > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        //Returns a string containing the names of all the enemies left in the room.
+        string EnemiesInRoom()
+        {
+            string enemiesInRoom = null;
+
+            for(int i = 0; i < player.currRoom.enemyList.Count; i++)
+            {
+                enemiesInRoom = enemiesInRoom + player.currRoom.enemyList[i].GetName() + " ";
+            }
+
+            return enemiesInRoom;
+        }
+        //When the enemy is killed, send a message saying they are dead, remove them from the room and output the enemies left in the room if any.
+        public string OnEnemyDead()
+        {
+            string message = null;
+            int enemyID = GetEnemeyPosInList();
+
+            message = enemy.GetName() + " has been slain! ";
+            player.currRoom.enemyList.Remove(player.currRoom.enemyList[enemyID]);
+
+            if(EnemiesLeftInRoom())
+            {
+                message = message + "The enemies in the room are now: " + EnemiesInRoom();
+            }
+            else
+            {
+                message = message + "You have cleared all enemies in the room.";
+            }
+
+            return message;
+        }
+        //Resets the player's health when they die and sends a message.
+        public string OnPlayerDeath()
+        {
+            string message = null;
+
+            message = player.name + " has died. They will now be sent back to the start of the dungeon.";
+            player.playerHealth.SetHealth(100);
+
+            return message;
+        }
+
+        public string EnemyAttackPlayer(int damageToDeal)
+        {
+            string message = null;
+
+            enemy.EnemyAttack(player, damageToDeal);
+
+            message = player.name + " has been attacked by " + enemy.GetName() + " for " + damageToDeal + ". This player now has " + player.playerHealth.GetHealth() + " health. ";
+
+            return message;
+        }
+
+        //Variables
+        public Player player;
+        public Enemy enemy;
+    }
+}
---
+++ MUD/Server/code/MoveRoomManager.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Server
+{
+    public class MoveRoomManager
+    {
+        public MoveRoomManager(Player player, string[] input)
+        {
+            this.player = player;
+            this.input = input;
+        }
+
+        //Checks if the enemy count in the current room is more than 0.
+        public bool CheckForEnemiesInRoom()
+        {
+            bool enemiesInRoom = false;
+
+            if (player.currRoom.enemyList.Count > 0)
+            {
+                enemiesInRoom = true;
+            }
+
+            return enemiesInRoom;
+        }
+
+        //Returns a string with a list of enemy names in the current room
+        string GetListOfEnemies()
+        {
+            string message = null;
+
+            for(int i = 0; i < player.currRoom.enemyList.Count; i++)
+            {
+                message = message + player.currRoom.enemyList[i].GetName() + " ";
+            }
+
+            return message;
+        }
+
+        //Returns a string containing all players names currently in the room.
+        public string GetPlayersInRoom(List<Player> playerList)
+        {
+            string message = null;
+            bool playerInRoom = false;
+
+            for(int i = 0; i < playerList.Count; i++)
+            {
+                if(playerList[i].currRoom == player.currRoom && playerList[i] != player)
+                {
+                    if (!playerInRoom)
+                    {
+                        playerInRoom = true;
+                        message = "You see the following people in the room: ";
+                    }
+                    message = message + playerList[i].name + " ";
+                }
+            }
+
+            return message;
+        }
+
+        //Returns a string to send to players about the enemies in a room.
+        public string ListEnemiesInRoom()
+        {
+            string message = null;
+            if (CheckForEnemiesInRoom())
+            {
+                message = "You see several enemies in the room: " + GetListOfEnemies();
+            }
+
+            return message;
+        }
+
+        //Returns a string with all possible directions in the current room.
+        public string GetPossibleDirections()
+        {
+            string message = "You can go ";
+
+            if (player.currRoom.north != null)
+            {
+                message = message + " north";
+            }
+            if (player.currRoom.south != null)
+            {
+                message = message + " south";
+            }
+            if (player.currRoom.east != null)
+            {
+                 message = message + " east ";
+            }
+            if (player.currRoom.west != null)
+            {
+                message = message + " west.";
+            }
+
+            return message;
+        }
+
+        //Checks if the input direction is valid. Moves to room if so. Returns a error message if false.
+        public string MoveRoom(Dictionary<String, Room> roomMap)
+        {
+            string message = null;
+
+            if (input[1].ToLower() == "north" && player.currRoom.north != null)
+            {
+                player.currRoom = roomMap[player.currRoom.north];
+            }
+            else if (input[1].ToLower() == "south" && player.currRoom.south != null)
+            {
+                player.currRoom = roomMap[player.currRoom.south];
+            }
+            else if (input[1].ToLower() == "east" && player.currRoom.east != null)
+            {
+                player.currRoom = roomMap[player.currRoom.east];
+            }
+            else if (input[1].ToLower() == "west" && player.currRoom.west != null)
+            {
+                player.currRoom = roomMap[player.currRoom.west];
+            }
+            else
+            {
+                message = "ERROR, you cannot go that way.";
+            }
+
+            return message;
+        }
+        //Variables
+        Player player;
+        string[] input;
+    }
+}
